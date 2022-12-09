a0cdff53335f20445fffb5854c82fbc49c36eb59
Justice <justice@hubtel.com>
2022-12-09 09:29:47 +0000

    add about page custom fields

--- template-parts/content-about.php
+++ template-parts/content-about.php
@@ -4,56 +4,39 @@
                 <div class="breadcrumb-content mt-4">
                     <h2>About Us</h2>
                     <ul>
-                        <li><a href="<?php echo site_url();?>">Home</a></li>
+                        <li><a href="<?php echo site_url(); ?>">Home</a></li>
                         <li class="active">About Us</li>
                     </ul>
                 </div>
             </div>
         </div>
         <!-- Kenne's Breadcrumb Area End Here -->
-        
+
         <!-- Begin Kenne's About Us Area -->
         <div class="about-us-area">
             <div class="container">
+
                 <div class="row">
                     <div class="col-lg-6 col-md-5">
                         <div class="overview-img text-center img-hover_effect">
-                            <a href="#">
-                                <img class="img-full" src="<?php echo get_template_directory_uri();?>/html-v2/assets/images/about-us/1.jpg" alt="">
-                            </a>
+                            <!-- <a href="#"> -->
+                            <img class="img-full" src="<?php echo the_field('banner'); ?>" alt="">
+                            <!-- </a> -->
                         </div>
                     </div>
                     <div class="col-lg-6 col-md-7 d-flex align-items-center">
                         <div class="overview-content">
-                            <h2>Welcome To <span>Ami's</span> Store!</h2>
-                            <p class="short_desc">We Provide Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                                Repudiandae nisi fuga facilis, consequuntur, maiores eveniet voluptatum, omnis possimus
-                                temporibus aspernatur nobis animi in exercitationem vitae nulla! Adipisci ullam illum quisquam.
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, nulla veniam? Magni aliquid
-                                asperiores magnam. Veniam ex tenetur.</p>
+                            <h2><?php echo the_field('title'); ?></h2>
+                            <p class="short_desc"><?php echo the_field('short_description'); ?></p>
                             <div class="kenne-about-us_btn-area">
-                                <a class="about-us_btn" href="#">Shop Now</a>
+                                <a class="about-us_btn" href="<?php echo the_field('button_url'); ?>"><?php echo the_field('button_text'); ?></a>
                             </div>
                         </div>
                     </div>
                 </div>
 
                 <div class="my-5">
-                        <p>We Provide Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        Repudiandae nisi fuga facilis, consequuntur, maiores eveniet voluptatum, omnis possimus
-                        temporibus aspernatur nobis animi in exercitationem vitae nulla! Adipisci ullam illum quisquam.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, nulla veniam? Magni aliquid
-                        asperiores magnam. Veniam ex tenetur.</p>
-                        <p>We Provide Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        Repudiandae nisi fuga facilis, consequuntur, maiores eveniet voluptatum, omnis possimus
-                        temporibus aspernatur nobis animi in exercitationem vitae nulla! Adipisci ullam illum quisquam.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, nulla veniam? Magni aliquid
-                        asperiores magnam. Veniam ex tenetur.</p>
-                        <p>We Provide Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        Repudiandae nisi fuga facilis, consequuntur, maiores eveniet voluptatum, omnis possimus
-                        temporibus aspernatur nobis animi in exercitationem vitae nulla! Adipisci ullam illum quisquam.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, nulla veniam? Magni aliquid
-                        asperiores magnam. Veniam ex tenetur.</p>
+                    <?php echo the_content(); ?>
                 </div>
             </div>
         </div>
