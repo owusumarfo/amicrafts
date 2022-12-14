f5d210ddd9aff444f212ccf1c703e35ea4ecf719
Justice <justice@hubtel.com>
2022-12-14 17:05:13 +0000

    add size guide

--- html-v2/assets/js/custom.js
+++ html-v2/assets/js/custom.js
@@ -1,6 +1,7 @@
 (function ($) {
 	"use strict";
 
+	// ----- Show/Hide personalized characters feature on product details page
 	$("#personalize_item_choice").on("change", () => {
 		const choice = $("#personalize_item_choice").val();
 
@@ -30,7 +31,7 @@
 		}
 	});
 
-	("http://amicrafts.test/wp-content/themes/amicrafts");
+	//--------Add size guide on product details page
 	const sizeGuideImage = `http://${location.hostname}/wp-content/themes/amicrafts/html-v2/assets/images/product/size-guide.png`;
 	const sizeGuideHtml = `<br> <a class="text-muted small" href="#" data-bs-toggle="modal" data-bs-target="#sizeGuideModal">Size Guide</a>`;
 	const sizeGuideModal = `
@@ -38,6 +39,11 @@
 	<div class="modal fade" id="sizeGuideModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 		<div class="modal-dialog modal-lg">
 			<div class="modal-content">
+				<div class="modal-header border-0 p-0">
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+						<i class="ion-android-close text-white"></i>
+					</button>
+				</div>
 				<div class="modal-body">
 					<img src="${sizeGuideImage}" alt="size guide"/>
 				</div>
